eebe92f08b1e2cbde1201640179a6eca95143bb8
flower <flower@k1024.de>
2015-02-05 01:42:37 +0100

    license info
    small public domain header

--- exchanges/poloniex.js
+++ exchanges/poloniex.js
@@ -1,3 +1,13 @@
+/*
+
+This is free and unencumbered software released into the public domain.
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+*/
+
 /*jslint node: true */
 /*jslint nomen: true */
 /*jslint plusplus: true */
